d5e61319f7ee4f9d126cea71c67d77c909b29f40
janmano <mrammanohur@gmail.com>
2021-09-03 14:44:05 +0400

    done boundary analysis with 4 criteria e.g totalIsTooBig

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -9,7 +9,7 @@ public class NumFinder {
 
             if(n < smallest)
                 smallest = n;
-            else if (n > largest)
+              else if (n > largest)
                 largest = n;
 
         }
